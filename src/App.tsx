76b7b705bfe4b049988b6a1037dadc31d308be74
Andrew Vasquez <andrewvasquez0113@gmail.com>
2025-03-19 20:54:51 -0700

    updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 import ResponsiveIframe from "./components/iframe";
 import ParticleBackground from "./components/particle-bg";
+import { Input } from "./components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 
 function App() {
+  const [url, setUrl] = useState<string | null>(null);
   return (
     <div className="w-screen h-screen relative">
       <Tabs
@@ -11,23 +14,21 @@ function App() {
         className="relative flex z-50 bg-transparent items-center p-4 min-h-screen"
       >
         <ParticleBackground />
+        <Input
+          onChange={(e) => setUrl(e.currentTarget.value)}
+          type="text"
+          placeholder="Url"
+          className="absolute left-5 top-5 min-w-fit w-50"
+        />
         <TabsList>
           <TabsTrigger value="phone">Phone</TabsTrigger>
           <TabsTrigger value="tablet">Tablet</TabsTrigger>
         </TabsList>
         <TabsContent value="phone">
-          <ResponsiveIframe
-            height={844}
-            width={390}
-            url="http://localhost:5174"
-          />
+          {url && <ResponsiveIframe height={844} width={390} url={url} />}
         </TabsContent>
         <TabsContent value="tablet">
-          <ResponsiveIframe
-            height={820}
-            width={1180}
-            url="http://localhost:5174"
-          />
+          {url && <ResponsiveIframe height={820} width={1180} url={url} />}
         </TabsContent>
       </Tabs>
     </div>
